052ea1b848de428469a1bb57d54a44e98b7e5e3a
Damian Liew Cho Xiang <damiancxliew@gmail.com>
2024-11-17 20:58:19 +0800

    Updated codes in Signup and Login pages, fixed navbar rendering

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,17 @@
-import { Outlet } from "react-router-dom";
-// import Navbar from "./components/Navbar";
-const App = () => {
-    return (
-        <div className="w-full pt-4">
-            <Outlet />
-        </div>
-    );
-};
-export default App;
+import { Outlet, useLocation } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
-// <Navbar /> //
+const App = () => {
+  const location = useLocation();
+  const hideNavbarRoutes = ["/login", "/signup"]; // Define routes where the Navbar should be hidden
+  const shouldShowNavbar = !hideNavbarRoutes.includes(location.pathname);
+
+  return (
+    <div className="w-full pt-4">
+      {shouldShowNavbar && <Navbar />}
+      <Outlet />
+    </div>
+  );
+};
+
+export default App;
