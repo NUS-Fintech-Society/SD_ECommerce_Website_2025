6eeea55b3ac187b356d59637e6e76e4b03e918d6
Damian Liew Cho Xiang <damiancxliew@gmail.com>
2025-02-24 11:41:46 +0800

    Fixed the error on image paths for Image Sizing Chart, fixed logic errors or Edit Listing + Add to Cart, added Price fields to all items

--- server/src/models/orders.js
+++ server/src/models/orders.js
@@ -1,18 +1,24 @@
 const mongoose = require("mongoose");
 
 const orderSchema = new mongoose.Schema({
-  userID: { type: String, required: true },
-  firstName: { type: String, required: true },
-  lastName: { type: String, required: true },
-  address: { type: String, required: true },
-  city: { type: String, required: true },
-  country: { type: String, required: true },
-  zipCode: { type: String, required: true },
-  items: [{
-    product: { type: mongoose.Schema.Types.ObjectId, ref: "Product", required: true },
-    quantity: { type: Number, required: true },
-  }],
-  createdDate: { type: Date, required: true },
+    userID: { type: String, required: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    address: { type: String, required: true },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    zipCode: { type: String, required: true },
+    items: [
+        {
+            product: {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: "Product",
+                required: true,
+            },
+            quantity: { type: Number, required: true },
+        },
+    ],
+    createdDate: { type: Date, required: true },
 });
 
 const Order = mongoose.model("Order", orderSchema);
--- server/src/models/products.js
+++ server/src/models/products.js
@@ -1,35 +1,42 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
-  },
-  description: {
-    type: String,
-    required: true,
-  },
-  images: [{
-    type: String, // Assuming images are stored as URLs
-    required: true
-  }],
-  sizingChart: [{
-    type: String, // Assuming images are stored as URLs
-    required: true
-  }],
-  specifications: [{
-    colour: { type: String, required: true },
-    size: { type: String, required: true },
-    quantity: { type: String, required: true }
-  }],
-  deliveryMethods: {
-    shipping: { type: Boolean, required: true },
-    selfCollection: { type: Boolean, required: true }
-  },
-  collectionInfo: {
-    type: String,
-    required: true,
-  }
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    images: [
+        {
+            type: String, // Assuming images are stored as URLs
+            required: true,
+        },
+    ],
+    sizingChart: [
+        {
+            type: String, // Assuming images are stored as URLs
+            required: true,
+        },
+    ],
+    specifications: [
+        {
+            colour: { type: String, required: true },
+            size: { type: String, required: true },
+            quantity: { type: String, required: true },
+            price: { type: String, required: true },
+        },
+    ],
+    deliveryMethods: {
+        shipping: { type: Boolean, required: true },
+        selfCollection: { type: Boolean, required: true },
+    },
+    collectionInfo: {
+        type: String,
+        required: true,
+    },
 });
 
 const Product = mongoose.model("Product", productSchema);
