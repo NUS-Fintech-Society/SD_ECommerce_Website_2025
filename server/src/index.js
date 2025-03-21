d0aa735b8edd51de3064d7fa6175f130301a9575
Damian Liew Cho Xiang <damiancxliew@gmail.com>
2025-03-21 17:47:20 +0800

    To allow handling of deployment on vercel

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,8 @@
-const express = require('express')
-require('dotenv').config();
+const express = require("express");
+require("dotenv").config();
 const isAuth = require("./middleware/auth");
 const bodyParser = require("./middleware/body_parser");
-const cors = require('cors');
+const cors = require("cors");
 
 const product = require("./routes/product_route");
 const user = require("./routes/user_route");
@@ -14,23 +14,22 @@ const userVerification = require("./routes/user_verification_route");
 const adminRequest = require("./routes/admin_request_route");
 const admin = require("./routes/admin_route");
 
-
-
 // Importing the mongooose package
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const app = express();
-const port = process.env.PORT;
+const isProduction = process.env.NODE_ENV === "production";
+const frontendURL = process.env.FRONTEND_URL || "http://localhost:3000";
 
-const corsOptions ={
-    origin:'*', 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200,
-}
+const corsOptions = {
+    origin: isProduction ? frontendURL : "*",
+    credentials: true, //access-control-allow-credentials:true
+    optionSuccessStatus: 200,
+};
 
 mongoose
     .connect(`${process.env.MONGO_URI}`) // Changing to MongoAtlas //Use localhost for now
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch((err) => console.error('Could not connect to MongoDB...'));
+    .then(() => console.log("Connected to MongoDB..."))
+    .catch((err) => console.error("Could not connect to MongoDB..."));
 
 app.use(express.json());
 app.use(cors(corsOptions));
@@ -47,6 +46,12 @@ app.use("/drafts", draftsRoute);
 app.use("/adminRequest", adminRequest);
 app.use("/admin", admin);
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-})
+if (!isProduction) {
+    const port = process.env.PORT || 5000;
+    app.listen(port, () => {
+        console.log(`🚀 Server running locally on port ${port}`);
+    });
+}
+
+// Export app for Vercel deployment
+module.exports = app;
